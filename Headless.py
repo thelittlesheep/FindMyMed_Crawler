487211e518f05f2c9e1301a9db0b6bd3a3cb183c
thelittlesheep <h3439149@gmail.com>
2019-10-03 13:44:27 +0800

    2019_10_03 解析完成
    完成藥庫soup之解析

--- Headless.py
+++ Headless.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 def main():
 	chrome_options = Options() # 啟動無頭模式
 	chrome_options.add_argument('--headless')  #規避google bug
 	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--window-size=1920,3240')
 	url = 'https://www.nhi.gov.tw/QueryN/Query1.aspx'
 	executable_path = 'chromedriver.exe'#自行設定路徑
 	driver = webdriver.Chrome(executable_path=executable_path,chrome_options=chrome_options)
 	driver.get(url)
-	
+	driver.find_element_by_id('ctl00_ContentPlaceHolder1_tbxName').send_keys('metformin')
+	driver.find_elements_by_name('ctl00$ContentPlaceHolder1$tbxPageNum')
+	#driver.find_elements_by_xpath("//form[@name='開始查詢'][@type='submit']")
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	driver.save_screenshot('sc.png')
+
 	driver.quit()#關閉瀏覽器 
 
 if __name__ == '__main__':
--- NHIdataCrawler.py
+++ NHIdataCrawler.py
@@ -32,7 +32,7 @@ def get_page(URL,drugName):
 		'ctl00$ContentPlaceHolder1$ddlIsUpdate': '',
 		'ctl00$ContentPlaceHolder1$tbxACT': '',
 		'ctl00$ContentPlaceHolder1$rblType': '迄今',
-		'ctl00$ContentPlaceHolder1$tbxPageNum': '10',
+		'ctl00$ContentPlaceHolder1$tbxPageNum': '1000',
 		'ctl00$ContentPlaceHolder1$btnSubmit': '開始查詢'
 	}
 
@@ -62,7 +62,7 @@ def get_pagenum(drugName,URL):
 
 	return len(pages_no)
 
-def get_result(drugName,URL,page):
+def get_result(drugName,URL):
 	soup = get_page(URL,drugName)
 
 	druglist = []
@@ -80,68 +80,29 @@ def get_result(drugName,URL,page):
 		raw_druglink.append(link.get('href'))
 	for i in range(6,(6+len(druglist))):
 		druglink.append(raw_druglink[i])
-	'''for i in range (0,len(druglist)):
+	for i in range (0,len(druglist)):
 		k = i+1
 		print('查詢序號:'+str(k))
 		print('藥物名稱:'+druglist[i][1])
 		print('藥物中文名稱:'+druglist[i][2])
 		print('藥物詳細資料:'+druglink[i])
-		print(' ')'''
-	for i in range(0,len(druglist)):
+		print(' ')
+	'''for i in range(0,len(druglist)):
 		k = page+len(druglist)
-		full_druglist.append([k,druglist[i][1],druglist[i][2],druglink[i]])
+		full_druglist.append([k,druglist[i][1],druglist[i][2],druglink[i]])'''
 
 	return full_druglist
 
+def get_input():
+	ch = input('請輸入欲查詢之藥品英文名稱:')
+	if ch == '':
+		return ch
+
+
 def main():
-	drugName = 'METFORMIN'
+	drugName = 'CLINDAMYCIN'
 	URL='https://www.nhi.gov.tw/QueryN/Query1.aspx'
-	form = {
-		'__VIEWSTATE': get_hiddenvalue(URL)[0],
-		'__VIEWSTATEGENERATOR': get_hiddenvalue(URL)[1],
-		'__SCROLLPOSITIONX': 0,
-		'__SCROLLPOSITIONY': 0,
-		'__EVENTTARGET': '',
-		'__EVENTARGUMENT': '',
-		'__VIEWSTATEENCRYPTED': '',
-		'ctl00$ContentPlaceHolder1$tbxNameChinese': '',
-		'ctl00$ContentPlaceHolder1$tbxName': drugName,
-		'ctl00$ContentPlaceHolder1$tbxQ1ID': '',
-		'ctl00$ContentPlaceHolder1$tbxDrugForm': '',
-		'ctl00$ContentPlaceHolder1$ddlDrugClassifyCode': '',
-		'ctl00$ContentPlaceHolder1$tbxElementName': '',
-		'ctl00$ContentPlaceHolder1$tbxElementQuantity': '',
-		'ctl00$ContentPlaceHolder1$tbxElementUnit': '',
-		'ctl00$ContentPlaceHolder1$tbxStandNum': '',
-		'ctl00$ContentPlaceHolder1$tbxStandUnit': '',
-		'ctl00$ContentPlaceHolder1$tbxSales': '',
-		'ctl00$ContentPlaceHolder1$ddlMixture': '',
-		'ctl00$ContentPlaceHolder1$ddlStartYYY': '',
-		'ctl00$ContentPlaceHolder1$ddlStartMM': '',
-		'ctl00$ContentPlaceHolder1$ddlMedType': '',
-		'ctl00$ContentPlaceHolder1$ddlIsUpdate': '',
-		'ctl00$ContentPlaceHolder1$tbxACT': '',
-		'ctl00$ContentPlaceHolder1$rblType': '迄今',
-		'ctl00$ContentPlaceHolder1$tbxPageNum': '10',
-		'ctl00$ContentPlaceHolder1$btnSubmit': '開始查詢'
-	}
-
-	chrome_options = Options() # 啟動無頭模式
-	chrome_options.add_argument('--headless')  #規避google bug
-	chrome_options.add_argument('--disable-gpu')
-	#executable_path = 'chromedriver.exe'#自行設定路徑
-	#driver = webdriver.Chrome(executable_path=executable_path,chrome_options=chrome_options)
-
-	driver = webdriver.Chrome('chromedriver.exe') # 創建一個driver用於打開網頁，記得找到brew安裝的chromedriver的位置，在創建driver的時候指定這個位置
-	final_druglist = []
-	page = 0
-	while page < get_pagenum(drugName, URL):
-		driver.get(URL)
-		drugEngName = driver.find_element_by_id('ctl00_ContentPlaceHolder1_tbxName')
-		drugEngName.sendKeys(drugName)
-		driver.find_element_by_id('ctl00_ContentPlaceHolder1_btnSubmit')
-		page = page + 1
-		time.sleep(2)
+	get_result(drugName, URL)
 
 if __name__ == '__main__':
     main()
